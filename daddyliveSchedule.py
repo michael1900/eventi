486dedd0fe56abcb809e2b326040498fb22a41a9
“emaschi5” <emanuele.schiano@gmail.com>
2025-01-31 23:46:28 +0000

    test

--- daddyliveSchedule.py
+++ daddyliveSchedule.py
@@ -7,44 +7,34 @@ import os
 import datetime
 import pytz
 
-#generate static list of static channel names
-#get schedule JSON
-#parse wanted tags
-    #build channel list
-    #build guide
-
-NUM_CHANNELS        = 400
-DADDY_JSON_FILE     = "daddyliveSchedule.json"
-M3U8_OUTPUT_FILE    = "daily.m3u8"
-EPG_OUTPUT_FILE     = "daily.xml"
-LOGO                = "https://raw.githubusercontent.com/JHarding86/daddylive-m3u/refs/heads/main/hardingtv.png"
+# Costanti
+NUM_CHANNELS = 10000
+DADDY_JSON_FILE = "daddyliveSchedule.json"
+M3U8_OUTPUT_FILE = "daily.m3u8"
+EPG_OUTPUT_FILE = "daily.xml"
+LOGO = "https://raw.githubusercontent.com/emaschi5/daddylive/refs/heads/main/stremioita.png"
 
 mStartTime = 0
 mStopTime = 0
 
+# Funzioni
 def generate_unique_ids(count, seed=42):
     random.seed(seed)
     ids = []
     for _ in range(count):
-        id_str = str(uuid.UUID(int=random.getrandbits(128)))  # Generate a unique UUID
+        id_str = str(uuid.UUID(int=random.getrandbits(128)))
         ids.append(id_str)
     return ids
 
 def loadJSON(filepath):
-    # Open and read the JSON file
     with open(filepath, 'r', encoding='utf-8') as file:
         json_object = json.load(file)
-
-    # Print the Python dictionary
-    # print(json_object)
-
     return json_object
 
 def createSingleChannelEPGData(UniqueID, tvgName):
-     #Creating M3U8 Data
-    xmlChannel      = ET.Element('channel')
-    xmlDisplayName  = ET.Element('display-name')
-    xmlIcon         = ET.Element('icon')
+    xmlChannel = ET.Element('channel')
+    xmlDisplayName = ET.Element('display-name')
+    xmlIcon = ET.Element('icon')
 
     xmlChannel.set('id', UniqueID)
     xmlDisplayName.text = tvgName
@@ -56,17 +46,15 @@ def createSingleChannelEPGData(UniqueID, tvgName):
     return xmlChannel
 
 def createSingleEPGData(startTime, stopTime, UniqueID, channelName, description):
-    #Creating EPG Data
-    programme   = ET.Element('programme')
-    title       = ET.Element('title')
-    desc        = ET.Element('desc')
+    programme = ET.Element('programme')
+    title = ET.Element('title')
+    desc = ET.Element('desc')
 
-    programme.set('start', startTime + " +0000")
-    programme.set('stop', stopTime + " +0000")
+    programme.set('start', str(startTime) + " +0000")  # Correzione qui
+    programme.set('stop', str(stopTime) + " +0000")    # Correzione qui
     programme.set('channel', UniqueID)
 
     title.text = channelName
-
     desc.text = description
 
     programme.append(title)
@@ -74,69 +62,61 @@ def createSingleEPGData(startTime, stopTime, UniqueID, channelName, description)
 
     return programme
 
-def addChannelsByLeagueSport(leagueSportTuple):
+def addChannelsByLeagueSport():
+    global channelCount
     for day, value in dadjson.items():
         try:
-            # print("NEW DAY\n\n\n")
-            for leagueSport in leageSportTuple:
-                sport = dadjson[f"{day}"][leagueSport["sport"]]
+            for sport in dadjson[day].values():
                 for game in sport:
-                    if leagueSport["league"] in game["event"]:
-                        print(game["event"])
-                    
-                        for channel in game["channels"]:
-                            #Creating Time Data
-                            date_time = day.replace("th ", " ").replace("rd ", " ").replace("st ", " ").replace("nd ", " ").replace("Dec Dec", "Dec")
-                            date_time = date_time.replace("-", game["time"] + " -")
-                            # Parse the cleaned date string into a datetime object
-                            date_format = "%A %d %b %Y %H:%M - Schedule Time UK GMT"
-                            start_date = datetime.datetime.strptime(date_time, date_format)
-                            global mStartTime
-                            mStartTime = start_date.strftime("%Y%m%d000000")
-                            stop_date = start_date + datetime.timedelta(days=2)
-                            global mStopTime
-                            mStopTime = stop_date.strftime("%Y%m%d000000")
-                            
-                            format_12_hour = start_date.strftime("%m/%d/%y")
-                            start_date = start_date - datetime.timedelta(hours=7)
+                    for channel in game["channels"]:
+                        date_time = day.replace("th ", " ").replace("rd ", " ").replace("st ", " ").replace("nd ", " ").replace("Dec Dec", "Dec")
+                        date_time = date_time.replace("-", game["time"] + " -")
+                        date_format = "%A %d %b %Y %H:%M - Schedule Time UK GMT"
+                        start_date_utc = datetime.datetime.strptime(date_time, date_format)
 
-                            startHour = start_date.strftime("%I:%M %p") + " (MST)"
-                            format_12_hour = format_12_hour + " - " + startHour
+                        amsterdam_timezone = pytz.timezone("Europe/Amsterdam")
+                        start_date_amsterdam = start_date_utc.replace(tzinfo=pytz.utc).astimezone(amsterdam_timezone)
 
-                            UniqueID    = unique_ids.pop(0)
-                            try:
-                                channelName = game["event"] + " " + format_12_hour + " " + channel["channel_name"]
-                            except TypeError:
-                                print("Ill formatted JSON, skipping this channel for this game.")
-                                continue
-                            
-                            channelID   = f"{channel['channel_id']}"
+                        mStartTime = start_date_amsterdam.strftime("%Y%m%d%H%M%S")
+                        stop_date_amsterdam = start_date_amsterdam + datetime.timedelta(days=2)
+                        mStopTime = stop_date_amsterdam.strftime("%Y%m%d%H%M%S")
 
-                            global channelCount
-                            tvgName = "OpenChannel" + str(channelCount).zfill(3)
-                            tvLabel = tvgName
-                            channelCount = channelCount + 1
+                        formatted_date_time_cet = start_date_amsterdam.strftime("%m/%d/%y")
+                        startHour = start_date_amsterdam.strftime("%H:%M") + " (CET)"
+                        formatted_date_time_cet = formatted_date_time_cet + " - " + startHour
 
-                            # tvgName = channelName
-                            # tvLabel = channel["channel_name"]
+                        UniqueID = unique_ids.pop(0)
+                        try:
+                            channelName = game["event"] + " " + formatted_date_time_cet + " " + channel["channel_name"]
+                        except TypeError:
+                            print("JSON mal formattato, canale saltato per questa partita.")
+                            continue
 
-                            with open(M3U8_OUTPUT_FILE, 'a', encoding='utf-8') as file:  # Use 'a' mode for appending
-                                file.write(f'#EXTM3U url-tvg="https://raw.githubusercontent.com/emaschi/daddylive/refs/heads/main/daily.xml"\n')        
-                                file.write(f'#EXTINF:-1 tvg-id="{UniqueID}" tvg-name="{tvgName}" tvg-logo="{LOGO}" group-title="Live", {tvLabel}\n')
-                                file.write(f'#EXTVLCOPT:http-referrer=https://ilovetoplay.xyz/\n')
-                                file.write(f'#EXTVLCOPT:http-user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1\n')
-                                file.write(f'#EXTVLCOPT:http-origin=https://ilovetoplay.xyz\n')
-                                file.write(f"https://xyzdddd.mizhls.ru/lb/premium{channelID}/index.m3u8\n")
-                                file.write('\n')
+                        channelID = f"{channel['channel_id']}"
 
-                            #Creating M3U8 Data
-                            xmlChannel = createSingleChannelEPGData(UniqueID, tvgName)
-                            root.append(xmlChannel)
+                        global channelCount
+                        tvgName = channelName
+                        tvLabel = tvgName
+                        channelCount = channelCount + 1
 
-                            programme = createSingleEPGData(mStartTime, mStopTime, UniqueID, channelName, "No Description")
-                            root.append(programme)
+                        with open(M3U8_OUTPUT_FILE, 'a', encoding='utf-8') as file:
+                            if channelCount == 1:
+                                file.write('#EXTM3U url-tvg="https://raw.githubusercontent.com/emaschi/daddylive/refs/heads/main/daily.xml"\n')
+
+                            file.write(f'#EXTINF:-1 tvg-id="{UniqueID}" tvg-name="{tvgName}" tvg-logo="{LOGO}" group-title="Live", {tvLabel}\n')
+                            file.write(f'#EXTVLCOPT:http-referrer=https://ilovetoplay.xyz/\n')
+                            file.write(f'#EXTVLCOPT:http-user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1\n')
+                            file.write(f'#EXTVLCOPT:http-origin=https://ilovetoplay.xyz\n')
+                            file.write(f"https://xyzdddd.mizhls.ru/lb/premium{channelID}/index.m3u8\n")
+                            file.write('\n')
+
+                        xmlChannel = createSingleChannelEPGData(UniqueID, tvgName)
+                        root.append(xmlChannel)
+
+                        programme = createSingleEPGData(mStartTime, mStopTime, UniqueID, channelName, "No Description")
+                        root.append(programme)
         except KeyError as e:
-            print(f"KeyError: {e} - One of the keys {day} or {leagueSportTuple} does not exist.")
+            print(f"KeyError: {e} - Una delle chiavi {day} non esiste.")
 
 channelCount = 0
 unique_ids = generate_unique_ids(NUM_CHANNELS)
@@ -150,19 +130,13 @@ if os.path.isfile(M3U8_OUTPUT_FILE):
 
 root = ET.Element('tv')
 
-#league sport tuple
-leageSportTuple = []
-leageSportTuple.append({"league":"NHL", "sport":"Ice Hockey"})
-leageSportTuple.append({"league":"NFL", "sport":"Am. Football"})
+addChannelsByLeagueSport()
 
-addChannelsByLeagueSport(leageSportTuple)
-
-#Fill out the remaining channels so that you don't have to re-add the channels list into plex
 for id in unique_ids:
-    with open(M3U8_OUTPUT_FILE, 'a', encoding='utf-8') as file:  # Use 'a' mode for appending
+    with open(M3U8_OUTPUT_FILE, 'a', encoding='utf-8') as file:
         channelNumber = str(channelCount).zfill(3)
         tvgName = "OpenChannel" + channelNumber
-        file.write(f'#EXTINF:-1 tvg-id="{id}" tvg-name="{tvgName}" tvg-logo="{LOGO}" group-title="USA (DADDY LIVE)", {tvgName}\n')
+        file.write(f'#EXTINF:-1 tvg-id="{id}" tvg-name="{tvgName}" tvg-logo="{LOGO}" group-title="Live", {tvgName}\n')
         file.write(f'#EXTVLCOPT:http-referrer=https://ilovetoplay.xyz/\n')
         file.write(f'#EXTVLCOPT:http-user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1\n')
         file.write(f'#EXTVLCOPT:http-origin=https://ilovetoplay.xyz\n')
